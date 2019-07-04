2766e3df3020924611ffea889dc0ed83478432c3
NdumisoMagunga <magunga55@gmail.com>
2019-07-04 01:27:08 -0700

    done initializing my state object on App component

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
 import SeasonDisplay from './SeasonDisplay';
 
-class App extends React.Component {
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            lat: null,
+            
+        };
+    }
     render() {
         window.navigator.geolocation.getCurrentPosition(
         (position) => console.log(position),
