7b0f595845fe6dac29882a4114435d2ce139fe00
NdumisoMagunga <magunga55@gmail.com>
2019-07-09 04:52:07 -0700

    done with conditional rendering of our lat, error, and loading state

--- src/index.js
+++ src/index.js
@@ -7,18 +7,33 @@ class App extends Component {
     constructor(props) {
         super(props);
 
+        // This is the only time we do direct assignment
+        // to  this.state
         this.state = {
             lat: null,
-            
+            errorMessage: '',
+
         };
+        window.navigator.geolocation.getCurrentPosition(
+        (position) => {
+            this.setState({ lat: position.coords.latitude})
+        },
+        (err) => {
+            this.setState({ errorMessage: err.message });
+        }
+    );
     }
     render() {
-        window.navigator.geolocation.getCurrentPosition(
-        (position) => console.log(position),
-        (err) => console.log(err)
-    );
-        return <div>Latitude: </div>
-    }
-}
+        if (this.state.errorMessage && !this.state.lat) {
+            return <div>Error: {this.state.errorMessage}</div>
+        }
 
+        if (!this.state.errorMessage && this.state.lat) {
+            return <div>Latitude: {this.state.lat}</div>
+        }
+
+            return <div>Loading...</div>
+    }
+};
+//xhilisi
 ReactDOM.render( <App />, document.querySelector("#root"));
