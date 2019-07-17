b0b249884745739fa024d88201ae75d0c0a6ad6b
NdumisoMagunga <magunga55@gmail.com>
2019-07-17 13:01:39 -0700

    added a spinner and good styling on my weather App, and a spinner to make it run smoothly

--- src/index.js
+++ src/index.js
@@ -2,38 +2,40 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
 import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
 class App extends Component {
-    constructor(props) {
-        super(props);
+    state = { lat: null, errorMessage: ''};
 
-        // This is the only time we do direct assignment
-        // to  this.state
-        this.state = {
-            lat: null,
-            errorMessage: '',
-
-        };
-        window.navigator.geolocation.getCurrentPosition(
-        (position) => {
-            this.setState({ lat: position.coords.latitude})
-        },
-        (err) => {
-            this.setState({ errorMessage: err.message });
-        }
-    );
+    componentDidMount() {
+         window.navigator.geolocation.getCurrentPosition(
+        position => this.setState({ lat: position.coords.latitude}),
+        err => this.setState({ errorMessage: err.message })  
+        );
     }
-    render() {
+
+    componentDidUpdate(){
+        console.log('My component re-rendered, just updated ');
+    }
+
+    renderContent() {
         if (this.state.errorMessage && !this.state.lat) {
             return <div>Error: {this.state.errorMessage}</div>
         }
 
         if (!this.state.errorMessage && this.state.lat) {
-            return <div>Latitude: {this.state.lat}</div>
+            return <SeasonDisplay lat={this.state.lat} />
         }
 
-            return <div>Loading...</div>
+        return <Spinner message="Please accept location request" />
+    }
+    render() {
+        return (
+            <div className="border red">
+                {this.renderContent()}
+            </div>
+        )
     }
 };
-//xhilisi
+//xhikiswa 201600 
 ReactDOM.render( <App />, document.querySelector("#root"));
